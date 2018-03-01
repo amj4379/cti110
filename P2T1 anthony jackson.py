d3c3f8e1b8fc382648e013acb35a1ff1f3a40db4
amj4379 <35510317+amj4379@users.noreply.github.com>
2018-03-01 18:43:06 -0500

    Add files via upload

---
+++ P2T1 anthony jackson.py
@@ -0,0 +1,20 @@
+#CTI-110
+#p2t1 sales prediction
+#Anthony Jackson
+#2/20/18
+
+
+
+#Projected Sales Totals
+
+
+totalSales = float(input("Enter Projected Sales $       "))
+
+
+#calculate the profit as 23 percent of totalSales
+
+profit = totalSales * 0.23
+
+#show profit
+
+print ("The profit is $", format (profit,",.2f" ))
