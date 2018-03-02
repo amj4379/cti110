8b1983b21cb581b3980f9509d743279298d5ca36
amj4379 <35510317+amj4379@users.noreply.github.com>
2018-03-01 20:29:15 -0500

    Add files via upload

---
+++ P2HW1_DistanceTraveled_JacksonAnthony.py
@@ -0,0 +1,23 @@
+#CTI-110
+#P2HW1-Distance Traveled
+#Anthony Jackson
+#02/20/2018
+
+
+#Distance = speed * time
+
+speed = 70
+print ("A car is traveling at 70 miles per hour")
+
+print ("In 6 hours, the car will travel " + str( ( speed * 6) ) )
+
+print ("In 10 hours, the car will travel " + str( ( speed * 10) ) )
+
+print ("In 15 hours, the car will travel " + str( ( speed * 15) ) )
+
+
+
+
+
+
+
